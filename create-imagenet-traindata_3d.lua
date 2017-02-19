903f7be2040bc951abb2df95ae94e0250c408aa4
ClaraBing <liubb16@connect.hku.hk>
2017-02-19 13:29:49 +0800

    data for zf_3d: 65817 imgs; prepare 3 files for each entry s.t. shuffle can be used

---
+++ create-imagenet-traindata_3d.lua
@@ -0,0 +1,207 @@
+-- specify the base path of the ILSVRC2015 dataset: 
+ILSVRC2015_BASE_DIR = '/disk/bingbin/ILSVRC2015_sampled_double/'
+
+require 'lfs'         -- Lua file system
+require 'LuaXML'      -- if missing use luarocks install LuaXML
+require 'utilities'
+require 'Rect' 
+
+local ground_truth = {}
+local class_names = {}
+local class_index = {}
+
+local file_cnt = 0
+
+function import_file(anno_base, data_base, fn, name_table)
+  local x = xml.load(fn)
+  local a = x:find('annotation')
+  local folder = a:find('folder')[1]
+  local filename = a:find('filename')[1]
+  local src = a:find('source')
+  local db = src:find('database')[1]
+  local sz = a:find('size')
+  local w = tonumber(sz:find('width')[1])
+  local h = tonumber(sz:find('height')[1])
+  
+  for _,e in pairs(a) do
+    if e[e.TAG] == 'object' then
+    
+      local obj = e
+      local name = obj:find('name')[1]
+      local bb = obj:find('bndbox') 
+      local xmin = tonumber(bb:find('xmin')[1])
+      local xmax = tonumber(bb:find('xmax')[1])
+      local ymin = tonumber(bb:find('ymin')[1])
+      local ymax = tonumber(bb:find('ymax')[1])
+      
+      if not class_index[name] then
+        class_names[#class_names + 1] = name
+        class_index[name] = #class_names 
+      end 
+      
+      -- Modified on Feb 19th: add fn names beforehand s.t. can use shuffle
+      local fidx = tonumber(fn:split('/')[9]:sub(1,6))
+      local prev_fidx, next_fidx = fidx, fidx
+      local fn_prefix = fn:sub(1, -11)
+      local prev_fn, next_fn
+      local offset_cnt = 0
+      while prev_fidx >= 0 do -- Mark
+        prev_fidx = prev_fidx - 1
+        prev_fn = fn_prefix .. string.format('%06d', prev_fidx) .. '.xml'
+        if io.open(prev_fn, 'r') ~= nil then
+          break
+        end
+        print(prev_fn .. ' skipped (prev)')
+        prev_fn = ''
+        offset_cnt = offset_cnt + 1
+        if offset_cnt == 4 then -- frames too far away should not be considered
+          break
+        end
+      end -- while
+      if prev_fn == '' then
+        prev_fn = fn
+      end
+      offset_cnt = 0
+      while next_fidx >= 0 do -- Mark
+        next_fidx = next_fidx + 1
+        next_fn = fn_prefix .. string.format('%06d', next_fidx) .. '.xml'
+        if io.open(next_fn, 'r') ~= nil then
+          break
+        end
+        print(next_fn .. ' skipped (next)')
+        next_fn = ''
+        offset_cnt = offset_cnt + 1
+        if offset_cnt == 4 then -- frames too far away should not be considered
+          break
+        end
+      end -- while
+      if next_fn == '' then
+        next_fn = fn
+      end
+
+      local fn_lst = {[1]=prev_fn, [2]=fn, [3]=next_fn}
+      local image_path = {}
+      for i=1,3 do
+        print('fn_lst[' .. i .. ']: ' .. fn_lst[i])
+        -- generate path relative to annotation dir and join with data dir
+        image_path[i] = path.join(data_base, path.relpath(fn_lst[i], anno_base))  
+      
+        -- replace 'xml' file ending with 'JPEG'
+        image_path[i] = string.sub(image_path[i], 1, #image_path[i] - 3) .. 'JPEG'    
+        print('image_path[' .. i .. ']: ' .. image_path[i])
+      end -- for
+      print('\n\n')
+
+--      print('fn')
+--      print(fn)
+--      print('anno_base')
+--      print(anno_base)
+--      print('image_path')
+--      print(image_path)
+      
+      table.insert(name_table, image_path)
+      print('succeffully imported ' .. image_path[2])
+      
+      local roi = {
+        rect = Rect.new(xmin, ymin, xmax, ymax),
+        class_index = class_index[name],
+        class_name = name
+      }
+      
+      local file_entry = ground_truth[image_path]
+      if not file_entry then
+        file_entry = { image_file_name = image_path, rois = {} }
+        ground_truth[image_path] = file_entry
+      end
+      -- print(file_entry) -- Mark
+      table.insert(file_entry.rois, roi)
+    end
+  end
+end -- import_file
+
+function import_directory(anno_base, data_base, directory_path, recursive, name_table)
+   for fn in lfs.dir(directory_path) do
+    local full_fn = path.join(directory_path, fn)
+    local mode = lfs.attributes(full_fn, 'mode') 
+    if recursive and mode == 'directory' and fn ~= '.' and fn ~= '..' then
+      import_directory(anno_base, data_base, full_fn, true, name_table)
+      collectgarbage()
+    elseif mode == 'file' and string.sub(fn, -4):lower() == '.xml' then
+      import_file(anno_base, data_base, full_fn, name_table)
+      file_cnt = file_cnt + 1
+      -- return -- Mark
+    end
+    if #ground_truth > 10 then
+      print('ground truth files > 10.\nReturning.')
+      return
+    end
+  end
+  return l
+end
+
+-- recursively search through training and validation directories and import all xml files
+function create_ground_truth_file(dataset_name, base_dir, train_annotation_dir, val_annotation_dir, train_data_dir, val_data_dir, background_dirs, output_fn)
+  function expand(p)
+    return path.join(base_dir, p)
+  end
+  
+  local training_set = {}
+  local validation_set = {}
+  import_directory(expand(train_annotation_dir), expand(train_data_dir), expand(train_annotation_dir), true, training_set)
+  import_directory(expand(val_annotation_dir), expand(val_data_dir), expand(val_annotation_dir), true, validation_set)
+  local file_names = keys(ground_truth)
+
+  -- Modified on Feb 12th: sort img files
+--  table.sort(training_set)
+--  table.sort(validation_set)
+--  table.sort(ground_truth)
+  
+  -- compile list of background images
+  local background_files = {}
+--  for i,directory_path in ipairs(background_dirs) do
+--    directory_path = expand(directory_path)
+--    for fn in lfs.dir(directory_path) do
+--      local full_fn = path.join(directory_path, fn)
+--      local mode = lfs.attributes(full_fn, 'mode')
+--      if mode == 'file' and string.sub(fn, -5):lower() == '.jpeg' then
+--        table.insert(background_files, full_fn)
+--      end
+--    end
+--  end
+  
+  print(string.format('Total images: %d; classes: %d; train_set: %d; validation_set: %d; (Background: %d)', 
+    #file_names, #class_names, #training_set, #validation_set, #background_files
+  ))
+  save_obj(
+    output_fn,
+    {
+      dataset_name = dataset_name,
+      ground_truth = ground_truth,
+      training_set = training_set,
+      validation_set = validation_set,
+      class_names = class_names,
+      class_index = class_index,
+      background_files = background_files
+    }
+  )
+  print('Done.')
+  print('file count = ' .. file_cnt)
+end -- create_ground_truth_file
+
+
+background_folders = {}
+for i=0,10 do
+  table.insert(background_folders, 'Data/VID/train/' .. i)
+end
+
+create_ground_truth_file(
+  'ILSVRC2015_VID',
+  ILSVRC2015_BASE_DIR,
+  'Annotations/VID/train', 
+  'Annotations/VID/val', -- Note: validation data is also from VID train set
+  'Data/VID/train',
+  'Data/VID/val',
+  background_folders,
+  -- 'ILSVRC2015_VID_test.t7'
+  'data_mine/ILSVRC2015_VID_sampled_double_test.t7'
+)
