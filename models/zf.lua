b85d5583ce416993e9788b49117664e276bc6efe
ClaraBing <liubb16@connect.hku.hk>
2017-03-06 17:34:16 +0800

    original zf structure

--- models/zf.lua
+++ models/zf.lua
@@ -4,14 +4,16 @@ function zf(cfg)
   -- layer here means a block of one or more convolution layers followed by a max-pooling layer
   -- Modified on Jan 24th: +2 parameters for stride & whether to pool
   local layers = { 
-    -- Conv1
-    { filters= 96, kW=7, kH=7, padW=3, padH=3, dropout=0.0, conv_steps=1, stride=2, maxPool=true},
-    -- Conv2
-    { filters=256, kW=5, kH=5, padW=2, padH=2, dropout=0.4, conv_steps=1, stride=2, maxPool=true},
-    -- Conv 3, 4
-    { filters=384, kW=3, kH=3, padW=1, padH=1, dropout=0.4, conv_steps=2, stride=1, maxPool=false},
-    -- Conv 5
-    { filters=256, kW=3, kH=3, padW=1, padH=1, dropout=0.4, conv_steps=1, stride=1, maxPool=false};
+    -- Conv1 + ReLU + LRN + Pooling
+    { filters= 96, kW=7, kH=7, padW=3, padH=3, dropout=0.0, conv_steps=1, stride=2, LRN=true, maxPool=true},
+    -- Conv2 + ReLU + LRN + Pooling
+    { filters=256, kW=5, kH=5, padW=2, padH=2, dropout=0.0, conv_steps=1, stride=1, LRN=true, maxPool=true},
+    -- Conv 3 + ReLU
+    { filters=384, kW=3, kH=3, padW=1, padH=1, dropout=0.0, conv_steps=1, stride=1, LRN=false, maxPool=false},
+    -- Conv 4 + ReLU
+    { filters=384, kW=3, kH=3, padW=1, padH=1, dropout=0.0, conv_steps=1, stride=1, LRN=false, maxPool=false},
+    -- Conv 5 + ReLU
+    { filters=256, kW=3, kH=3, padW=1, padH=1, dropout=0.0, conv_steps=1, stride=1, LRN=false, maxPool=false};
   }
   
   local anchor_nets = {
@@ -20,6 +22,10 @@ function zf(cfg)
     { kW=5, n=256, input=4 },
     { kW=7, n=256, input=4 }
   }
+--  Modified on Mar 3rd: only 1 AnchorNetworks from the last conv layer
+--  local anchor_nets = {
+--      { kW=3, n=256, input=5}
+--  }
   
   local class_layers =  {
     { n=1024, dropout=0.5, batch_norm=true },
